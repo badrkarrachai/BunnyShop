6100d906ea0f49e89d44932e3e581c1d06c541ec
Badr Karrachai <badrkarrachai1999@gmail.com>
2024-01-25 14:10:16 +0100

    Adding product card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ProductCard from '@/components/Product/ProductCard'
 import Image from 'next/image'
 
 export default function Home() {
   return (
   <>
   <h1>bunny Shop</h1>
+  <ProductCard/>
   
   </>
   )
