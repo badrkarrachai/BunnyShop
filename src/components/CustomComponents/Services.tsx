a2ad795a8a0b6d28392cb07891ed3a97ea93f66c
Badr Karrachai <badrkarrachai1999@gmail.com>
2024-01-28 21:55:21 +0100

    Change Design

--- src/components/CustomComponents/Services.tsx
+++ src/components/CustomComponents/Services.tsx
@@ -5,12 +5,14 @@ interface Servicesprops{
 export default function Services(config: Servicesprops) {
   return (
     <div className="px-1 sm:px-12 flex-col flex gap-2 animate-DownToUp">
-        <div className="sm:text-3xl sm:px-0 px-2 text-[1.3rem] ml-4 font-bold text-[rgb(19,23,39)]">{config.title}</div>
-        <p className="ml-4 text-sm sm:px-0 px-2 text-blue-gray-500">{config.discreption}</p>
-        <div className="w-full flex justify-center   ">
-            <div className="bg-[#F1E4C3] rounded-3xl w-full h-96">
-                <div className="bg-[#B0926A] rounded-3xl absolute left-[2rem] max-w-[90rem] w-full h-[22rem] bottom-4"> </div>
-            </div>
+        <div className="sm:text-3xl sm:px-0 px-2 text-[1.3rem]  font-bold text-[rgb(19,23,39)]">{config.title}</div>
+        <p className="text-sm sm:px-0 px-2 text-blue-gray-500">{config.discreption}</p>
+        <div className="flex flex-col gap-8">
+          <div className="w-full flex gap-8">
+              <div className="bg-[#F1E4C3] rounded-[2rem] w-[65%] h-[23.5rem]"></div>
+              <div className="bg-[#B0926A] rounded-[2rem] w-[35%] h-[23.5rem]"></div>
+          </div>
+          <div className="bg-[#EDD8C7] rounded-[2rem] w-[100%] h-[23.5rem]"></div>
         </div>
     </div>
     
