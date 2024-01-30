9c832696d0fa64a391d78b034e722eccaf374ee0
Hanane Bouzaga <hananebouzaga38@gmail.com>
2024-01-30 01:38:46 +0100

    cart Panel adding

--- src/components/CustomComponents/CartPanel.tsx
+++ src/components/CustomComponents/CartPanel.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { IoClose } from "react-icons/io5";
 import OutsideAlerter from '../OutSideDetecter';
 import { motion } from "framer-motion"
+import { Button } from '@material-tailwind/react';
 
 interface CartPanelProps{
   isVisible:any;
@@ -13,14 +14,47 @@ function CartPanel(config:CartPanelProps) {
     <>
       <div className='w-full h-full fixed inset-0 backdrop-blur-sm bg-[#02020267] flex justify-end items-end phone:items-center z-30 animate-Fade '>
         <OutsideAlerter classOfIt="rounded-xl w-full panel:w-auto flex panel:justify-end h-full " setVisible={config.isVisible}>
-          <div className='h-full shadow-lg  bg-white w-full panel:w-[550px] p-8 animate-RightToLeft'>
-            <div className='border-b-2 mt-[-1rem] py-4 flex justify-between items-center'>
-              <h3 className='font-semibold text-lg'>Cart </h3>
-              <div onClick={()=>config.isVisible(false)}  className='hover:bg-gray-100 p-1 cursor-pointer rounded-full'><IoClose size="1.3rem" /></div>
+          <div className='h-full shadow-lg  bg-white w-full panel:w-[550px]  animate-RightToLeft'>
+            <div className='sm:px-8 px-5 pt-5 sm:pt-8'>
+              <div className='border-b-2 mt-[-1rem] py-4 flex justify-between items-center'>
+                <h3 className='font-semibold text-lg'>Cart (1) </h3>
+                <div onClick={()=>config.isVisible(false)}  className='hover:bg-gray-100 p-1 cursor-pointer rounded-full'><IoClose size="1.3rem" /></div>
+              </div>
             </div>
-            <div>
-              <CartItem image='/prodtest.jpg'/>
+            <div className='px-2 w-full h-[calc(100%-260px)]'>
+              <div className='overflow-y-scroll w-full h-full px-4'>
+                  <CartItem image='/prodtest.jpg'/>
+                  <CartItem image='/prodtest.jpg'/>
+                  <CartItem image='/prodtest.jpg'/>
+                  <CartItem image='/prodtest.jpg'/>
+                  <CartItem image='/prodtest.jpg'/>
+                  <CartItem image='/prodtest.jpg'/>  
+                  <CartItem image='/prodtest.jpg'/>
+                  <CartItem image='/prodtest.jpg'/>  
+                  <CartItem image='/prodtest.jpg'/>
+                  <CartItem image='/prodtest.jpg'/>
+               
+       
+              </div>
             </div>
+            <div className='sm:px-8 px-5  pt-4 flex gap-3 flex-col'>
+              <div className='flex flex-col gap-2'>
+                  <div className='flex justify-between items-center'>
+                      <span>Shipping</span>
+                      <span >Free</span>
+                  </div>
+                  <div className='flex justify-between items-center'>
+                      <span >Transaction fee</span>
+                      <span >$1.00</span>
+                  </div>
+                  <div className='flex justify-between items-center'>
+                      <span className='text-[rgb(8,9,15)] font-medium' >Total</span>
+                      <span className='text-[rgb(8,9,15)] font-medium'>$53.00</span>
+                  </div>
+              </div>
+              <Button size="md" placeholder={undefined} style={{textTransform:"initial"}} className="w-full bg-[#B0926A] h-12 text-base">Continue to checkout</Button>
+            </div>
+
             {/* <div className='flex justify-center items-center w-full mt-[-2rem] h-full flex-col text-[rgb(19,23,39)]'>
               <div className='sm:h-auto sm:w-auto w-40 h-40'><Image src={'/CartEmpty.png'} width={250} height={250} alt='Empty Cart Image'/></div>
               <div className='flex flex-col gap-2 justify-center items-center'>
