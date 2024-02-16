696a104b8ed7880f3f34d5d4913242eaf96e42e4
badr_karrachai <badrkarrachai1999@gmail.com>
2024-02-16 15:20:29 +0100

    gift cards

--- src/app/giftCards/page.tsx
+++ src/app/giftCards/page.tsx
@@ -1,11 +1,79 @@
+"use client"
 import GiftCards from "@/components/CustomComponents/GiftCards";
+import { Carousel } from "@material-tailwind/react";
 
 export default function page() {
-    return (
-      <>
-        <div className="max-w-[100rem]">
+  return (
+    <>
+      <div className='w-full flex justify-center items-center my-14'>
+        <div className="max-w-[100rem] flex flex-col gap-5 ">
+          <GiftCardsHero image="" title="" />
           <GiftCards/> 
         </div>
-      </>
-    )
-  }
+      </div>
+    </>
+  )
+}
+
+interface GiftCardsHeroProps{
+  image: string,
+  title: string,
+}
+
+function GiftCardsHero({ image, title}: GiftCardsHeroProps ){
+  return(
+    <>
+      <div className=" h-[360px] px-3 sm:px-10 animate-DownToUp">
+        <Carousel placeholder={""} transition={{ duration: 1 }} 
+        navigation={({ setActiveIndex, activeIndex, length }) => (
+          <div className="absolute bottom-4 left-2/4 flex -translate-x-2/4 gap-2 z-0">
+            {new Array(length).fill("").map((_, i) => (
+              <span
+                key={i}
+                className={`block h-2 cursor-pointer rounded-2xl transition-all content-[''] ${
+                  activeIndex === i ? "w-2 bg-white" : "w-2 bg-white/50"
+                }`}
+                onClick={() => setActiveIndex(i)}
+              />
+            ))}
+          </div>
+        )}
+        nextArrow={({ loop, handleNext, lastIndex }) => (
+          <button
+            onClick={handleNext}
+            disabled={!loop && lastIndex}
+            className="!absolute  top-2/4 right-1 sm:right-4 -translate-y-2/4 rounded-full disabled:hidden select-none transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-9 max-w-[48px] h-9 max-h-[48px] text-white hover:bg-white/10 active:bg-white/30 grid place-items-center"
+          >
+            <img src="chevron-right.svg" className="inline" alt="" />
+          </button>
+        )}
+        prevArrow={({ loop, handlePrev, firstIndex }) => {
+            return (
+              <button
+                onClick={handlePrev}
+                disabled={!loop && firstIndex}
+                className="!absolute top-2/4 left-1 sm:left-4 -translate-y-2/4 rounded-full disabled:hidden select-none transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-9 max-w-[48px] h-9 max-h-[48px] text-white hover:bg-white/10 active:bg-white/30 grid place-items-center"
+              >
+                <img src="chevron-left.svg" alt="" />
+              </button>
+            );
+          }}
+        className="rounded-xl">
+          <div className="w-full flex h-full">
+            <img
+              src="gift1.png"
+              alt="image 1"
+              className="h-full w-full  object-cover"
+            />
+            <div className="w-full h-full absolute opacity-20  bg-black"></div>
+            <div className="flex justify-center items-center w-full h-full absolute flex-col gap-4 p-4">
+              <h2 className="text-white text-4xl uppercase font-bold text-center">Special deal on our gift cards</h2>
+              <p className="text-center max-w-[38rem] text-white text-sm">BunnyShop gift cards are currently accessible! Get yours here with an amazing offer, or explore a variety of gift cards for different services.</p>
+            </div>
+          </div>
+          
+        </Carousel>
+      </div>
+    </>
+  )
+}
--- src/components/CustomComponents/GiftCards.tsx
+++ src/components/CustomComponents/GiftCards.tsx
@@ -9,20 +9,23 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import { AiOutlineShopping } from "react-icons/ai";
 
 
 export default function GiftCards() {
   return (
     <>
-        <div className="px-1 sm:px-6 flex-col flex gap-2 animate-DownToUp">
-          <div className="flex items-center justify-between">
-          <div className="sm:text-3xl text-[1.3rem] ml-4 font-bold text-[rgb(19,23,39)]">Gift Cards</div>
+        <div className="px-1 sm:px-6 flex-col flex gap-2 animate-DownToUp ">
+
           
-          </div>
-          <p className="ml-4 text-sm text-blue-gray-500">Here you can buy the latest gift cards in our website</p>
           <div className='flex flex-wrap w-full '>
-            <GiftCardsItem image="https://i.pinimg.com/564x/58/1e/76/581e76e832ee71c3cb93c5f4a47e7257.jpg" title="Amazon.com" priceRange1="5.00" priceRange2="100.00" value="Value" />
-            
+            <GiftCardsItem image="https://i.pinimg.com/originals/10/2e/f7/102ef79c7ee4e07bce8df227ce7b2da5.jpg" title="Amazon gift card" priceRange1="5.00" priceRange2="100.00" value="Value" />
+            <GiftCardsItem image="https://cdn.ome.lt/USYftGOGEHyYT2Ir_VQ9Z-g6J7k=/970x360/smart/uploads/conteudo/fotos/xbox_tzv1mNy_DXNIcPv.png" title="Xbox gift card" priceRange1="5.00" priceRange2="100.00" value="Value" />
+            <GiftCardsItem image="https://upload.wikimedia.org/wikipedia/commons/2/23/Uber_eats_logo_2017_06_22.jpg" title="Uber eats gift card" priceRange1="5.00" priceRange2="100.00" value="Value" />
+            <GiftCardsItem image="https://livecards.net/pl/apple-gift-card-10-eur-be-72851.png" title="Apple gift card" priceRange1="5.00" priceRange2="100.00" value="Value" />
+            <GiftCardsItem image="https://media-cdn.tripadvisor.com/media/photo-s/29/4d/2a/63/r.jpg" title="Starbucks gift card" priceRange1="5.00" priceRange2="100.00" value="Value" />
+            <GiftCardsItem image="https://corporate.target.com/getmedia/aff90129-8b00-4a07-8abd-4d3839661020/2022-Review-Header_Target.jpg" title="Target gift card" priceRange1="5.00" priceRange2="100.00" value="Value" />
+            <GiftCardsItem image="https://mexico-now.com/wp-content/uploads/2023/03/walmart-market.jpg" title="Walmart gift card" priceRange1="5.00" priceRange2="100.00" value="Value" />
           </div>
         </div>
     </>
@@ -42,7 +45,6 @@ function GiftCardsItem({image, title, priceRange1, priceRange2, value}:GiftCards
         <>
             <div className="flex hover:bg-gray-100 rounded-xl  cursor-pointer group flex-col h-auto to2:w-[33.33%] to3:w-[25%] to4:w-[20%] prod1880:w-[20%] w-[50%] p-2 sm:p-4">
               <div className="flex justify-end">
-             
               </div>
               <div className="sm:h-[14rem] h-[11rem] md:h-[270px]">
                 <img
@@ -52,21 +54,30 @@ function GiftCardsItem({image, title, priceRange1, priceRange2, value}:GiftCards
                             />
               </div>
               <div className="flex flex-col p-2 mt-2 gap-5">
-                  <div className="flex flex-col gap-2">
-                      <div className="font-semibold text-[rgb(19,23,39)] text-sm sm:text-base">{title}</div>
-                      <div className="font-bold text-base flex items-center">${priceRange1} - ${priceRange2}</div>
-                  </div>
-                  <div className="text-blue-gray-400 text-sm sm:text-base">{value}</div>
-                  <Select>
-                      <SelectTrigger className="w-[45%]">
-                        <SelectValue placeholder="Theme" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="light">Light</SelectItem>
-                        <SelectItem value="dark">Dark</SelectItem>
-                        <SelectItem value="system">System</SelectItem>
-                      </SelectContent>
-                    </Select>
+                <div className="flex flex-col gap-2">
+                    <div className="font-semibold text-[rgb(19,23,39)] text-sm sm:text-base">{title}</div>
+                    <div className="font-bold text-base flex items-center">${priceRange1} - ${priceRange2}</div>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <div className="text-blue-gray-400 text-sm ">{value}</div>
+                    <div className="flex sm:flex-row flex-col sm:justify-between gap-2 sm:items-center">
+                        <Select>
+                            <SelectTrigger className="sm:w-[45%] max-w-[185px]">
+                                <SelectValue placeholder="$100.00" />
+                            </SelectTrigger>
+                            <SelectContent >
+                                <SelectItem className="hover:bg-gray-100 hover:cursor-pointer" value="light">$5.00</SelectItem>
+                                <SelectItem className="hover:bg-gray-100 hover:cursor-pointer" value="dark">$20.00</SelectItem>
+                                <SelectItem className="hover:bg-gray-100 hover:cursor-pointer" value="system">$100.00</SelectItem>
+                            </SelectContent>
+                        </Select>
+                        <Button placeholder={""} style={{textTransform:"initial"}} variant="filled" className="flex items-center p-[0.62rem] w-full max-w-[125px] justify-center active:shadow-none hover:shadow-none bg-white text-black border shadow-none">
+                        <AiOutlineShopping  size="1.2rem"/>
+                            Add to cart
+                        </Button>
+                    </div>
+                </div>
+                  
                     
                   
                     
